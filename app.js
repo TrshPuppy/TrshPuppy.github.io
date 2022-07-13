ac690be5722b5a63f42547224448e5475ecbd336
Rose Pineau <rosepineau@protonmail.com>
2022-07-13 16:33:26 -0400

    Add event listener to logo. Add on hover change.

---
+++ app.js
@@ -0,0 +1,12 @@
+const logo = document.getElementById("logo");
+console.log(logo);
+
+logo.addEventListener("mouseenter", (e) =>
+{
+    e.target.style = "transform:scale(1.1,1.1);";
+});
+
+logo.addEventListener("mouseleave", (e) =>
+{
+    e.target.style = "transform:scale(1,1);";
+})
