7ef87a39d9bfe3992de0c6fc5ddf28599885740e
Rose Pineau <rosepineau@protonmail.com>
2022-07-25 10:25:19 -0400

    Fix comments, add semicolons.

--- contact.js
+++ contact.js
@@ -1,12 +1,12 @@
 // Retrieve contact-pic
 const contactPic = document.querySelector(".contact-pic");
 
-//Add hover event listener:
+// Add hover event listener:
 contactPic.addEventListener('mouseover', e =>
 {
     e.target.src = "./img/about1.jpg";
-})
+});
 contactPic.addEventListener('mouseout', e =>
 {
     e.target.src = "./img/about-pix.jpg";
-})
+});
