ccc77303418eef088fd297e9ef5a013c99e2bbda
Rose Pineau <rosepineau@protonmail.com>
2022-07-25 10:28:48 -0400

    Gix comments, add semicolons.

--- about.js
+++ about.js
@@ -1,12 +1,12 @@
-// Retrieve contact-pic
+// Retrieve about-pic
 const aboutPic = document.querySelector(".about-pic");
 
 //Add hover event listener:
 aboutPic.addEventListener('mouseover', e =>
 {
     e.target.src = "./img/contact1.jpg";
-})
+});
 aboutPic.addEventListener('mouseout', e =>
 {
     e.target.src = "./img/contact-pix.jpg";
-})
+});
