4b9bb17c2a700e88a3fc0264ffb30dbe4c70f232
Rose <trshpuppy@proton.me>
2023-07-21 12:10:35 +0000

    update.

--- main.go
+++ main.go
@@ -1,10 +1,23 @@
 package main
 
 import (
-	"net/http"
+//	"net/http"
 	"fmt"
 )
 
 func main(){
-	fmt.Println("Main has started...")	
+	fmt.Println("Main has started...")
+	Server := struct {
+		address string
+		port string
+	}{
+		address: "127.0.0.1",
+		port: "8080",
+	}
+
+	fmt.Printf("The server is running at %s:%s\n", Server.address, Server.port)
+	//fs := http.FileServer(http.Dir("assets/"))
+	//http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	//http.ListenAndServe()
 }
