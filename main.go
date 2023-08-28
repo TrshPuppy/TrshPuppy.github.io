8fa78913de91ceac203ac20805a43ecff9104229
Rose <trshpuppy@proton.me>
2023-08-28 02:23:58 +0100

    re-implement server, change port for new environment.

--- main.go
+++ main.go
@@ -15,19 +15,27 @@ func main(){
 		address string
 		port string
 	}{
-		address: "0.0.0.0",
+		address: "127.0.0.1",
 		port: "8080",
 	}
 
+	// Create the server/ multiplexer
 	HTTPMux := http.NewServeMux()
+
+	// // Tell the server which function to call to handle request to the root path
 	HTTPMux.HandleFunc("/", indexHandler)
 	
-	http.ListenAndServe(":" + Server.port, HTTPMux)
+	// http.HandleFunc("/", indexHandler)
+	http.ListenAndServe(Server.address + ":" + Server.port, HTTPMux)
 	
 	fmt.Printf("The server is running at %s:%s\n", Server.address, Server.port)
 
-	//fs := http.FileServer(http.Dir("assets/"))
-	//http.Handle("/static/", http.StripPrefix("/static/", fs))
+	// fs := http.FileServer(http.Dir("assets/"))
+	// http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	//http.ListenAndServe()
+	// http.ListenAndServe()
 }
+
+// Resource Links:
+// ** https://www.digitalocean.com/community/tutorials/how-to-make-an-http-server-in-go#multiplexing-request-handlers
+// ** https://pkg.go.dev/net/http#ListenAndServe
