6ab3d51cfff154d03e8063a11e2176eaaf37bc33
Rose <trshpuppy@proton.me>
2023-08-28 22:11:47 +0100

    abandon WriteAt()

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
+// 
+	// * INSTEAD of modifying file starting @ offset, 
+	// * just overwrite the whole thing:
+	// *** Copy <head> to <body> into string
+	// *** overwrite file w/:
+	// ***         copy_of_header string + content + closing_tags string
+	// *** closing_tags can be </body> + </html>
+	//
 package main
 
 import (
@@ -35,35 +43,61 @@ func aboutHandler(respWriter http.ResponseWriter, req *http.Request){
 
 	// modify index.html
 	// modify_index := modify_index_html() // this should return an HTML file/ change the index.html file in root dir
-	index_file, err := os.Open("index.html")
+	index_file, err := os.OpenFile("index.html", os.O_RDWR, 0666) // filemode == linux perms??
 	if err != nil {
 		fmt.Printf("ERROR: line 40 :)")
 		return
 	}
-	// ** file.WriteAt <-- writes starting at a specific length of bytes into file (need length offset)
-	// ** find byte offset:   file.Read()?  bufio.newScanner() <-- line by line
-	offset_line := "  <body>" // <-- probably should be something like "includes '<body" instead
-	scanner := bufio.NewScanner(index_file)
+	
+	
 
-	// loop through file line by line:
-	for scanner.Scan(){
-		// Read current line & save line number:
-		current_line_txt := scanner.Text()
+	// HERE
+
+
+
+	// // ** file.WriteAt <-- writes starting at a specific length of bytes into file (need length offset)
+	// // ** find byte offset:   file.Read()?  bufio.newScanner() <-- line by line
+	// offset_line := "  <body>" // <-- probably should be something like "includes '<body" instead
+	// scanner := bufio.NewScanner(index_file)
+	// bytes_offset := len(offset_line)
+
+	// // loop through file line by line to find offset byte:
+	// for scanner.Scan(){
+	// 	// Read current line & save line number:
+	// 	current_line_txt := scanner.Text()
 		
-		fmt.Printf(current_line_txt)
-		// Compare line to offset_line
-		if current_line_txt == offset_line{
-			// Get # bytes into file that offset pattern happens
-			fmt.Printf("wow biotch, u did it, the line is %s", len(scanner.Bytes()))
-			break
-		}
-	}
+	// 	fmt.Printf(current_line_txt)
+	// 	bytes_offset += len(scanner.Bytes())
+	// 	// Compare line to offset_line
+	// 	if current_line_txt == offset_line{
+	// 		// Get # bytes into file that offset pattern happens
+	// 		fmt.Printf("wow biotch, u did it, the line is %s", len(scanner.Bytes()))
+	// 		//byte_offset = len(scanner.Bytes())
+	// 		break
+	// 	}
+	// }
+	// fmt.Printf("byte offset var ", bytes_offset)
+
+	defer index_file.Close()
 
 	// Use byte offset to set where we edit file...
-	// starting_line := line_count++
+	// about_html = "    " + about_html
+	// content_length := len(about_html)
+	// if _, err := index_file.WriteAt([]byte(about_html), int64(byte_offset)); err != nil{
+	// 	fmt.Printf("ERROR: line 70 :) +", err)
+	// 	return
+	// }
+
+	index_file.WriteAt([]byte("\n" + about_html), int64(bytes_offset));
+
+	// Close file:
+	index_file.Close()
 
 
-	// // Create new template
+
+
+
+	// // Create new template:
 	// var about_tpl = template.Must(template.ParseFiles("index.html"))
 
 	// // Execute new template:
