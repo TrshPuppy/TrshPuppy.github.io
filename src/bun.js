48ceb8584eb8bf9b3279e6ea2d272eadeee342c4
TrshPuppy <trshpuppy@proton.me>
2023-10-19 22:16:08 -0400

    implementing bun + jsx."

--- src/bun.js
+++ src/bun.js
@@ -1,5 +1,7 @@
 // import Renderer from "./src/renderer.js";
-const file = Bun.file;
+//import { jsx } from "react/jsx-runtime"
+import renderRoot from "./index-page";
+// import { h } from "preact";
 
 // Create and run Bun HTTP server:
 const Server = {
@@ -7,42 +9,42 @@ const Server = {
   host: "0.0.0.0", // <-- change this for production, we're running in a VM...
   getResponse: (rURL) => {
     console.log("URL PATH: " + rURL.pathname);
-    // const splitPath = rURL.split("/");
-    // const fixedPath = splitPath[splitPath.length - 1];
-    // console.log(fixedPath);
-
-    switch (rURL.pathname) {
-      case "/portfolio": {
-        console.log("portfolio requested");
-        Server.endpoint.portfolio();
-        break;
-      }
-      case "/about": {
-        console.log("about requested");
-        return Server.endpoint.about();
-        break;
-      }
-      case "/contact": {
-        console.log("contact requested");
-        return Server.endpoint.contact();
-        break;
-      }
-      case "/": {
-        console.log("home requested");
-        return Server.endpoint.root();
-        break;
-      }
-      default: {
-        console.log("default");
-        Server.endpoint.root();
-        throw new Error("Request for non-existent path");
-        // return Server.endpoint.root();
-      }
-    }
+    return new Response(renderRoot());
+    // switch (rURL.pathname) {
+    //   case "/portfolio": {
+    //     console.log("portfolio requested");
+    //     Server.endpoint.portfolio();
+    //     break;
+    //   }
+    //   case "/about": {
+    //     console.log("about requested");
+    //     return Server.endpoint.about();
+    //     break;
+    //   }
+    //   case "/contact": {
+    //     console.log("contact requested");
+    //     return Server.endpoint.contact();
+    //     break;
+    //   }
+    //   case "/home": {
+    //     console.log("home requested");
+    //     return new Response(renderRoot());
+    //     // return Server.endpoint.root();
+    //     break;
+    //   }
+    //   // default: {
+    //   //   console.log("default");
+    //   //   return new Response(<div>TIDDIES</div>);
+    //   //   // Server.endpoint.root();
+    //   //   // throw new Error("Request for non-existent path");
+    //   //   // return Server.endpoint.root();
+    //   // }
+    // }
   },
 
   endpoint: {
     root: () => {
+      return renderRoot();
       console.log("root");
     },
     portfolio: () => {
@@ -73,9 +75,9 @@ const BunBridge = {
     await Bun.build({
       entrypoints: ep,
       outdir: od,
-      naming: {
-        asset: "[name].[ext]",
-      },
+      // naming: {
+      //   asset: "[name].[ext]",
+      // },
     });
   },
 };
--- src/index-page.jsx
+++ src/index-page.jsx
@@ -1,6 +1,14 @@
-import { Page } from "@nakedjsx/core/page";
+//import { Page } from "@nakedjsx/core/page";
+import { jsx } from "preact/jsx-runtime";
 
-Page.Create("en");
-Page.AppendHead(<title> tiddies </title>);
-Page.AppendBody(<p>wow</p>);
-Page.Render();
+// export default function renderRoot(){
+//     return <div>
+//         <p>TIDDIES ROOT</p>
+//         </div>;
+
+// }
+
+// const renderRoot = () => jsx(<div>{"TIDDIES"}</div>)
+const renderRoot = () => jsx("Div",{ color: "red" }, "Home")
+
+export default renderRoot;
