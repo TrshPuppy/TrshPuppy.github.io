3b06e97798ba56bfcc5d380a99b24f601f982176
TrshPuppy <trshpuppy@proton.me>
2023-10-28 16:29:28 -0400

    fix base path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import preact from "@preact/preset-vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   assetsInclude: ["./src/assets/*.png", "./src/assets/*.gif"],
-  base: "https://trshpuppy.github.io",
+  base: "./",
   plugins: [preact()],
 });
