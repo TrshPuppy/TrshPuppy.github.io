3852a098c43b8713eecda75a529cdda27a7b59cd
TrshPuppy <trshpuppy@proton.me>
2023-11-12 22:44:44 -0500

    writeup cards sort of work

--- src/endpoints/current-writeup.jsx
+++ src/endpoints/current-writeup.jsx
@@ -1,35 +1,68 @@
+import { useEffect, useState } from 'preact/hooks';
 import mdFilesList from '../data/md.json';
 import { Converter } from "showdown";
 
-export default function CurrentWriteup(requestedWriteup) {
-  // Get of requested markdown file:
-  const mdString = getMarkdownFile(requestedWriteup);
+// export default function CurrentWriteup(props) {
+//   console.log(`requested writeup = ${props.writeup}`)
 
-  const converter = new Converter();
-  const htmlString = converter.makeHtml(mdString);
+//   const mdPath = props.writeup;
+//   // Get of requested markdown file:
+//   const [data, setData] = useState(null);
 
-  const html = { __html: htmlString};
+//  useEffect(()=>{
+//   fetch(mdPath)
+//     .then(res => res.text())
+//     .then(mdString => {
+//     setData(mdString);
+//     })
+//   }, [mdPath]);
 
-  return (
-    <>
-      <div id="markdown-div" dangerouslySetInnerHTML={html}></div>
-    </>
-  );
-}
+//   // let html = { __html: "" };
+//   if(data === null){
+//     return <div>loading...</div>;
+   
+//   }else {
+//     const converter = new Converter();
+//     const htmlString = converter.makeHtml(data);
+  
+//     const html = { __html: htmlString};
 
-function getMarkdownFile(rqst){
-  // Rqst is just the name prop of the object in MD.json, use that to fetch the path
-  const writeupPath = () =>{
-    for(let file of mdFilesList) {
-      if(file.name === rqst) {
-        return file.path;
-      }
-    }
-  }
+//     return (
+//       <>
+//         <div id="markdown-div" dangerouslySetInnerHTML={html}></div>
+//       </>
+//     );
+//   }
 
-  // Get the markdown file:
-  const mdText = fetch(writeupPath()).then((res) => 
-    res.text());
+ 
 
-  return mdText;
-}
+
+// }
+
+// function getMarkdownPath(rqst){
+//   // Rqst is just the name prop of the object in MD.json, use that to fetch the path
+//   let writeupPath = "";
+//   for(let file of mdFilesList) {
+//     if(file.name === rqst) {
+//       writeupPath = file.path;
+//     }
+//   }
+
+//   // const writeupPath = () =>{
+//   //   for(let file of mdFilesList) {
+//   //     if(file.name === rqst) {
+//   //       return file.path;
+//   //     }
+//   //   }
+//   // }
+
+// import mdText from `${writeupPath}`;
+
+
+//   // // Get the markdown file:
+//   // const mdText = fetch(writeupPath()).then((res) => 
+//   //   res.text());
+
+//   // return mdText;
+//   return mdText;
+// }
--- src/endpoints/writeups.jsx
+++ src/endpoints/writeups.jsx
@@ -1,48 +1,36 @@
 import { useEffect, useState, useRef } from "preact/hooks";
-import Section from "../fragments/section";
-import getMarkdownFiles from "./markdown/md-parser";
 import mdFilesList from '../data/md.json';
-import CurrentWriteup from "./current-writeup";
+// import CurrentWriteup from "./current-writeup";
+import { Converter } from "showdown";
 
 export default function WriteUps() {
    // Set state related to the user's content choice:
   const [content, setContent] = useState('previews');
+  console.log(`content = ${content}`)
 
   // Build array of preview card components:
   const previewCards = [];
-    for(let file of mdFilesList) {
-      previewCards.push(<PreviewCard fileObj={file} setContent={setContent} />);
-    }
-
-  if(content === 'previews') {
-    return <div id="writeup-previews" className="section-row">{previewCards}</div>
-  } else {
-    return <CurrentWriteup writeup={content} />
+  for(let file of mdFilesList) {
+    console.log('file in preview card for loop = ${file}'+file.description)
+    previewCards.push(<PreviewCard fileObj={file} setContent={setContent} />);
   }
-  
 
   // useEffect(() => {
-  //   switch(content){
-  //     case 'previews':
-  //       setContent(<div id="writeup-previews" className="section-row">{previewCards}</div>);
-  //       break;
-  //     case `current-writeup`:
-  //       setContent(<CurrentWriteup writeup={content} >);
-  //       break;
 
+  // },[])
 
-  //   }
-  // },[setContent, content])
-
-
-  // console.log(mdFilesList[0]);
-
-  
-
-
+  if(content === 'previews') {
+    console.log("made it in here bitch")
+    return <div id="writeup-previews" className="section-row">{previewCards}</div>
+  } else {
+    console.log(`content else = ${content}`)
+    // return <div> fuck my life</div>
+    return <CurrentWriteup writeup={content} />
+  }
 }
 
-function PreviewCard({ fileObj, setContent }) {
+function PreviewCard({fileObj, setContent} ) {
+  console.log(`file description in previuew card func = ${fileObj.description}`)
   // Fix description length & title:
   if(fileObj.description.length > 40) {
     fileObj.description = fileObj.description.slice(0, 40) + "...";
@@ -55,7 +43,7 @@ function PreviewCard({ fileObj, setContent }) {
   return (
     <div className="preview-card section-row">
       <img className="wu-preview-img" src={fileObj.img_path} alt={fileObj.name} />
-      <div onClick={setContent(fileObj.name)} className="wu-preview-text section-column">
+      <div onClick={() => setContent(fileObj.path)} className="wu-preview-text section-column">
         <h2>{fileObj.name}</h2>
         <p>{fileObj.description}</p>
       </div>
@@ -63,6 +51,49 @@ function PreviewCard({ fileObj, setContent }) {
     </div>
   );
 }
+
+function CurrentWriteup(props) {
+  console.log(`requested writeup = ${props.writeup}`)
+
+  const mdPath = props.writeup;
+  // Get of requested markdown file:
+  const [data, setData] = useState(null);
+
+ useEffect(()=>{
+  fetch(mdPath)
+    .then(res => res.text())
+    .then(mdString => {
+    setData(mdString);
+    })
+  }, []);
+
+  // let html = { __html: "" };
+  if(data === null){
+    return <div>loading...</div>;
+   
+  }else {
+    const converter = new Converter();
+    const htmlString = converter.makeHtml(data);
+  
+    const html = { __html: htmlString};
+
+    return (
+      <>
+        <div id="markdown-div" dangerouslySetInnerHTML={html}></div>
+      </>
+    );
+  }
+
+ 
+
+
+}
+
+
+
+
+
+
 /*
    h1 HACKING WRITEUPS
    h2 Welcome to the dark side
