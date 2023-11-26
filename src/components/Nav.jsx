d59c5dcaca30864545eb34c96dd1790ba6acf0df
Bryan Patrick <bpatrick@ou.edu>
2023-11-25 21:55:52 -0600

    Added SVGs

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,4 +1,8 @@
 import { Link } from "react-router-dom";
+import CoffeeSVG from "./svg/Coffee.jsx";
+import FlaskSVG from "./svg/Flask.jsx";
+import HomeSVG from "./svg/Home.jsx";
+import PawSVG from "./svg/Paw.jsx";
 
 const Nav = () => {
     return (
@@ -6,16 +10,28 @@ const Nav = () => {
             <img src="../../public/img/logo-1.webp" alt="Trash Puppy Logo."/>
             <ul>
                 <li>
-                    <Link to={'/'} className="nav-link">Home</Link>
+                    <Link to={ '/' } className="nav-link">
+                        <span>Home</span>
+                        <HomeSVG/>
+                    </Link>
                 </li>
                 <li>
-                    <Link to={'/projects'} className="nav-link">Projects</Link>
+                    <Link to={ '/projects' } className="nav-link">
+                        <span>Projects</span>
+                        <FlaskSVG/>
+                    </Link>
                 </li>
                 <li>
-                    <Link to={'/about'} className="nav-link">About</Link>
+                    <Link to={ '/about' } className="nav-link">
+                        <span>About</span>
+                        <PawSVG/>
+                    </Link>
                 </li>
                 <li>
-                    <Link to={'/contact'} className="nav-link">Contact</Link>
+                    <Link to={ '/contact' } className="nav-link">
+                        <span>Contact</span>
+                        <CoffeeSVG/>
+                    </Link>
                 </li>
             </ul>
         </nav>
