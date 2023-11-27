c8d4fdfadbb503dab7530596873b3b0d2f392b13
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2023-11-27 00:09:58 -0600

    Added external link icons and reducer

---
+++ src/components/svg/CodeWars.jsx
@@ -0,0 +1,9 @@
+const CodeWarsSVG = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path d="M1 12H0v-2h1-1V9l1-1V7l1-1 1-1V4v1h1l1-1V3h1V2h1V1h3V0h2l1 1V0v1h1v1h5v1h2v1h1v3l1 1h-1v1h1v2h1v4h-1v2h-1v1h-1v1h-1v1h-1l-1 1v1h-1l-1 1h-1v1l-1-1v1h-3v-1H9v-1H7l-1-1v1a1 1 0 0 1-1-1H4l-1-1v-1H2l-1-1h1l-1-1v-5zm10-1-1-1V6h1V4h1V2h-2v1H8v2H7v4h1v1h1v1h2zm1-1V9h1V8h2V7h5l1 1V7h-1V5h-2V4h-3v1h-2v1h-1v3c-1 0-1 0 0 0v1zm8 7 1-1v-1h1l-1-1h1v-1h-1v-2h-1v-1h-2V9h-3v1h-2v1h3v1h2v2h1v2h1v1zm-7-5v1h1v4l-1 1v1h-1v1l-1 1h-1l1 1h3v-1h1v-1h1v-1l1-1v-3l-1-1v-1h-1l-1-1h-1zm-1 1-1 1-1 1-1 1H3v1c0 1 0 0 0 0l1 1h1v1h2v-1 1h3v-1h1v-1h1v-4zm-1-1H8l-1-1H6v-1L5 9V7H4V6v1H3v2l-1 1h1v2h1v1h1v1h1c1 0 0 0 0 0h3l1-1h1v-1z"/>
+        </svg>
+    )
+}
+
+export default CodeWarsSVG;
