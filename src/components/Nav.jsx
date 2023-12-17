913f4bbd236778ae0c2b0616211c0d198b1c1333
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2023-12-17 00:44:18 -0600

    Added html background parallax

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -21,7 +21,7 @@ const Nav = () => {
 
     const playClickSound = () => {
         const audio = new Audio('/audio/click.mp3');
-        audio.volume = 0.3;
+        audio.volume = 0.2;
         audio.play();
     };  
 
--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -1,14 +1,39 @@
+import React, { useState, useEffect, useRef } from 'react';
 import SiteMenu from "./SiteMenu.jsx";
 
-const Wrapper = ({View}) => {
-    return (
-        <>
-            <SiteMenu />
-            <main>
-                <View/>
-            </main>
-        </>
-    )
-}
+const Wrapper = ({ View }) => {
+    const mouseRef = useRef();
+    const [mouseX, setMouseX] = useState(0);
+    const [mouseY, setMouseY] = useState(0);
+
+    useEffect(() => {
+        const handleMouseMove = (event) => {
+            setMouseX(event.clientX);
+            setMouseY(event.clientY);
+        };
+
+        document.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+        };
+  }, []);
+
+  const calculateBackgroundPosition = () => {
+    const offsetX = (mouseX / window.innerWidth) * 3;
+    const offsetY = (mouseY / window.innerHeight) * 3;
+    return `${offsetX}% ${offsetY}%`;
+  };
+
+  return (
+    <>
+      <style>{`html {background-position: ${calculateBackgroundPosition()}}`}</style>
+      <SiteMenu />
+      <main ref={mouseRef}>
+        <View />
+      </main>
+    </>
+  );
+};
 
 export default Wrapper;
