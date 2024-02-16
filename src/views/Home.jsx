c11f636169a1e49f6a27e87a7cf11fd34d22d1b9
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2024-02-16 01:33:56 -0600

    Switching to terminal design

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,69 +1,40 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import Pong from '../components/Pong';
 
 const Home = () => {
-   const [amountOfFlashies] = useState(204);
-   const [flashyDec] = useState(Array.from({ length: amountOfFlashies }, () => Math.floor(Math.random() * 8)));
-   const [randomArr] = useState(getRandomIndexArr());
-   const [retroColors] = useState([
-      '#FF00FF',
-      '#9000FF',
-      '#6600FF',
-      '#00F2FF',
-      '#FF4287',
-      '#42FFEC',
-      '#C2E5CA',
-      '#FF0000',
-      '#FF249C',
-      '#F7FF66',
-   ]);
-
-   function getRandomNumber() {
-      const randomNumber = Math.random() * 6;
-      return Math.floor(randomNumber) + 4;
-   }
-
-   function getRandomIndexArr() {
-      const randomArr = [];
-
-      while (randomArr.length < 2) {
-         const index = Math.floor(Math.random() * flashyDec.length);
-
-         if (!randomArr.includes(index)) {
-            randomArr.push(index);
-         }
-      }
-
-      return randomArr;
-   }
+   const trafficLights = [
+      '#FF5E57',
+      '#FEBB2E',
+      '#28C740'
+   ];
 
    return (
       <article id="home-view" className="home">
-         <div className="masthead">
+         <div className="terminal">
+            <div className="traffic-lights" aria-hidden="true">
+               { trafficLights.map((colorIndex, i) => (
+                  <div
+                     key={ `flashy-${ i }` }
+                     className="traffic-light"
+                     style={ { color: `${ trafficLights[i] }` } }
+                  ></div>
+               )) }
+            </div>
             <div className="entry-title">
-               <div className="computer">
-                  <div className="computer-graphic"></div>
-                  <div className="dumpster-graphic"></div>
-                  <div className="flashy-deck" aria-hidden="true">
-                     {flashyDec.map((colorIndex, i) => (
-                        <div
-                           key={`flashy-${i}`}
-                           className={`flashy ${randomArr.includes(i) ? 'active' : ''}`}
-                           style={{ color: retroColors[colorIndex], animationDuration: `${getRandomNumber()}s` }}
-                           onClick={(e) =>
-                              e.target.classList.contains('active')
-                                 ? e.target.classList.remove('active')
-                                 : e.target.classList.add('active')
-                           }
-                        ></div>
-                     ))}
-                  </div>
-               </div>
                <h1 className="entry-title">Welcome to the Trash Heap.</h1>
                <div className="short-links">
-                  <NavLink to={'/blog'}>My Content</NavLink>
-                  <NavLink to={'/contact'}>Want to Chat?</NavLink>
+                  <NavLink to={ '/blog' }>
+                     <div>My Content</div>
+                     <img className="short-link-img" src="/animated/paper.webp" alt="" />
+                  </NavLink>
+                  <NavLink to={ '/contact' }>
+                     <div>Want to Chat?</div>
+                     <img className="short-link-img" src="/animated/tp-wag.webp" alt="" />
+                  </NavLink>
+                  <NavLink to={ '/projects' }>
+                     <div>My Projects</div>
+                     <img className="short-link-img" src="/animated/dumpster.gif" alt="" />
+                  </NavLink>
                </div>
             </div>
             <div className="hero"></div>
--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,11 +1,9 @@
 const Projects = () => {
     return (
         <article id="projects-view" className="view">
-
             <p>
                 Projects stuffs
             </p>
-
         </article>
     )
 }
