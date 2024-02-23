2c545c2589c87dd04f92dbad594294e773e25099
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2024-02-23 01:04:40 -0600

    Implementing projects filtering

--- src/views/Projects.jsx
+++ src/views/Projects.jsx
@@ -1,28 +1,74 @@
+import { useState } from 'react';
+
 const Projects = ({ projects }) => {
+   let topicSet = new Set();
+   let languageSet = new Set();
+
+   if (projects) {
+      let allTopics = [];
+      let allLanguages = [];
+
+      projects.forEach(project => {
+         const { topics, language } = project;
+
+         if (language) allLanguages.push(language);
+
+         if (topics.length > 0) {
+            topics.forEach(topic => {
+               allTopics.push(topic);
+            });
+         }
+      });
+
+      topicSet = new Set(allTopics);
+      languageSet = new Set(allLanguages);
+   }
+
+   console.log(topicSet.size);
+   console.log(languageSet.size);
 
    const mapProjects = () => {
       if (projects !== undefined) {
          return (
             <div className="projects-container">
-               <aside>
-                  <nav></nav>
+               <aside className="filters">
+                  <nav>
+                     { languageSet.size > 0 &&
+                        <ul className="language-list">
+                           {[...languageSet].map((topic, i) => {
+                              return (
+                                 <div key={`topic-${i}`}>{topic}</div>
+                              )
+                           })}
+                        </ul>
+                     }
+                     { topicSet.size > 0 &&
+                        <ul className="language-list">
+                           {[...topicSet].map((topic, i) => {
+                              return (
+                                 <div key={`topic-${i}`}>{topic}</div>
+                              )
+                           })}
+                        </ul>
+                     }
+                  </nav>
                </aside>
 
                <div className="projects">
                   {
                      projects.map((project, i) => {
-                           const {
-                              name,
-                              topics,
-                              updated_at,
-                              watchers_count,
-                              language,
-                              archive_url,
-                              clone_url,
-                              description,
-                              open_issues,
-                              watchers
-                           } = project;
+                        const {
+                           name,
+                           topics,
+                           updated_at,
+                           watchers_count,
+                           language,
+                           archive_url,
+                           clone_url,
+                           description,
+                           open_issues,
+                           watchers,
+                        } = project;
                         return (
                            <div key={ `project-${ i }` }>
                               <h2>{ name }</h2>
@@ -30,8 +76,10 @@ const Projects = ({ projects }) => {
                                  <div className="last-updated"><span>Last updated on: </span>{ updated_at }</div>
                                  <div className="watchers-count"><span>Watchers count: </span>{ watchers_count }</div>
                                  <div className="language"><span>Language: </span>{ language }</div>
-                                 <a className="archive-url" aria-label={`View the ${name} project on github`}>View it on GitHub</a>
-                                 <a className="open-issues" aria-label={`View the issues on github`}>{open_issues} open issues</a>
+                                 <a className="archive-url" aria-label={ `View the ${ name } project on github` }>View
+                                    it on GitHub</a>
+                                 <a className="open-issues"
+                                    aria-label={ `View the issues on github` }>{ open_issues } open issues</a>
                               </div>
                               <p className="description">{ description }</p>
                               <ul className="topics">
