9e397f94c805f19c9a18394074aa2a8dc265ba48
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2024-04-01 02:08:40 -0500

    Re-worked some styles and contact content

--- src/components/svg/Twitch.jsx
+++ src/components/svg/Twitch.jsx
@@ -1,10 +1,10 @@
 const TwitchSVG = () => {
-    return (
-        <svg id="twitchSVG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
-            <path d="M3 0 0 5v23h8v4h4l4-4h5l9-9V0H3zm23 17-5 5h-5l-4 4v-4H6V4h20v13z"/>
-            <path d="M19 8h3v8h-3V8zm-6 0h3v8h-3V8z"/>
-        </svg>
-    )
-}
+   return (
+      <svg id="twitchSVG" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32">
+         <path d="M3 0 0 5v23h8v4h4l4-4h5l9-9V0H3zm23 17-5 5h-5l-4 4v-4H6V4h20v13z" />
+         <path d="M19 8h3v8h-3V8zm-6 0h3v8h-3V8z" />
+      </svg>
+   );
+};
 
 export default TwitchSVG;
--- src/views/Contact.jsx
+++ src/views/Contact.jsx
@@ -1,6 +1,7 @@
 import TwitchSVG from '../components/svg/Twitch.jsx';
 import YouTubeSVG from '../components/svg/YouTube.jsx';
 import GitHubSVG from '../components/svg/GitHub.jsx';
+import TwitterSVG from '../components/svg/Twitter.jsx';
 import PageTitle from '../components/PageTitle.jsx';
 
 const Contact = () => {
@@ -9,7 +10,6 @@ const Contact = () => {
          <PageTitle title="Contact" color="#ff93b7" />
          <article id="contact-view" className="contact view">
             <section>
-               {/* <div className="hero hero-about"></div> */}
                <div className="info">
                   <h2>Want to Chat?</h2>
                   <p>I'm active on a few platforms, please feel free to contact me on any of them.</p>
@@ -17,7 +17,10 @@ const Contact = () => {
             </section>
             <section>
                <div className="info">
-                  <h2>Twitch</h2>
+                  <h2>
+                     <TwitchSVG />
+                     <span>Twitch</span>
+                  </h2>
                   <p>
                      Join us in our thriving Twitch community where we explore cybersecurity, programming, and a bit of
                      everything in between - from AI art horrors to coding, and even chair building. We have a large and
@@ -32,15 +35,21 @@ const Contact = () => {
                      aria-label="Twitch"
                   >
                      <span>Visit the Trash Heap on Twitch</span>
-                     <TwitchSVG />
                   </a>
                </div>
                <div className="hero hero-twitch"></div>
             </section>
             <section>
                <div className="info">
-                  <h2>GitHub</h2>
-                  <p>fjoawiejf wioejf a;iowjf</p>
+                  <h2>
+                     <GitHubSVG />
+                     <span>GitHub</span>
+                  </h2>
+                  <p>
+                     Dive into my repositories where you'll find a collection of hands-on learning projects designed to
+                     enhance my cybersecurity skills. From beginner-friendly exercises to more advanced challenges, each
+                     project offers valuable insights and practical experience.
+                  </p>
                   <a
                      className="link youtube"
                      data-name="YouTube"
@@ -50,49 +59,55 @@ const Contact = () => {
                      aria-label="Twitch"
                   >
                      <span>View my GitHub</span>
-                     <GitHubSVG />
                   </a>
                </div>
                <div className="hero hero-github"></div>
             </section>
             <section>
                <div className="info">
-                  <h2>Twitter</h2>
+                  <h2>
+                     <YouTubeSVG />
+                     <span>YouTube</span>
+                  </h2>
                   <p>
                      I keep a video blog documenting my journey and tech findings since I left my job as a nurse. Don't
                      forget to subscribe!
                   </p>
                   <a
+                     className="link youtube"
+                     data-name="YouTube"
+                     href="https://www.youtube.com/@TrshPuppy"
+                     target="_blank"
+                     rel="noreferrer"
+                     aria-label="YouTube"
+                  >
+                     <span>Watch my YouTube stuffs</span>
+                  </a>
+               </div>
+               <div className="hero hero-youtube"></div>
+            </section>
+            <section>
+               <div className="info">
+                  <h2>
+                     <TwitterSVG />
+                     <span>Twitter</span>
+                  </h2>
+                  <p>
+                     Follow me on Twitter, where you can get the latest news on my journey into cybersecurity and read
+                     about other fun stuff going on in my life and career.
+                  </p>
+                  <a
                      className="link twitter"
                      data-name="Twitter"
-                     href="https://www.youtube.com/@TrshPuppy"
+                     href="https://twitter.com/trshpuppy"
                      target="_blank"
                      rel="noreferrer"
                      aria-label="Twitter"
                   >
-                     <span>Watch my Twitter stuffs</span>
-                     <YouTubeSVG />
+                     <span>Get the latest news about the Trash Heap!</span>
                   </a>
                </div>
                <div className="hero hero-twitter"></div>
-            </section>
-            <section>
-               <div className="info">
-                  <h2>GitHub</h2>
-                  <p>Github stuffs</p>
-                  <a
-                     className="link github"
-                     data-name="GitHub"
-                     href="https://www.youtube.com/@TrshPuppy"
-                     target="_blank"
-                     rel="noreferrer"
-                     aria-label="GitHub"
-                  >
-                     <span>View my GitHub</span>
-                     <GitHubSVG />
-                  </a>
-               </div>
-               <div className="hero hero-github"></div>
             </section>
          </article>
       </>
