6f3bf79f495dfc01a8464c434b7ff02ee4b904cd
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2024-04-04 16:40:38 -0500

    correction

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
    plugins: [react()],
    assetsInclude: ['**/*.md'],
-   base: 'https://arthvadrr.github.io/TrshPuppy.github.io/dist/',
+   base: './',
 });
