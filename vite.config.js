23486913fc4160ccfab09d591a8b2fbbff733343
Bryan Patrick <54081465+arthvadrr@users.noreply.github.com>
2024-04-04 16:35:55 -0500

    Pathing

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
    plugins: [react()],
    assetsInclude: ['**/*.md'],
-   base: '/dist/',
+   base: '/TrshPuppy.github.io/dist/',
 });
