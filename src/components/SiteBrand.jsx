d5d317482527e273eaa0f787d776c31e0067e2c2
TrshPuppy <trshpuppy@proton.me>
2024-04-17 12:43:22 -0400

    add tribute to arth!

--- src/components/SiteBrand.jsx
+++ src/components/SiteBrand.jsx
@@ -1,9 +1,9 @@
 const SiteBrand = () => {
-    return (
-        <div className="site-brand" aria-hidden="true">
-            <div className="site-brand-img"></div>
-        </div>
-    )
-}
+   return (
+      <div className="site-brand" aria-hidden="true">
+         <div className="site-brand-img"></div>
+      </div>
+   );
+};
 
 export default SiteBrand;
